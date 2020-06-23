a87f2dcffa7fc46ff4fb98141cd0a3efe13eb8f8
CLT <1330525233@qq.com>
2020-06-23 17:48:08 +0800

    antd demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+
+import { Result, Button, Typography } from 'antd'
+import { CloseCircleOutlined } from '@ant-design/icons'
+
+import 'antd/dist/antd.css'
+
+const { Paragraph, Text } = Typography
+
+
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
+    <Result
+      status="error"
+      title="Submission Failed"
+      subTitle="Please check and modify the following information before resubmitting."
+      extra={[
+        <Button type="primary" key="console">
+          Go Console
+      </Button>,
+        <Button key="buy">Buy Again</Button>,
+      ]}
+    >
+      <div className="desc">
+        <Paragraph>
+          <Text
+            strong
+            style={{
+              fontSize: 16,
+            }}
+          >
+            The content you submitted has the following error:
+        </Text>
+        </Paragraph>
+        <Paragraph>
+          <CloseCircleOutlined className="site-result-demo-error-icon" /> Your account has been
+        frozen. <a>Thaw immediately &gt;</a>
+        </Paragraph>
+        <Paragraph>
+          <CloseCircleOutlined className="site-result-demo-error-icon" /> Your account is not yet
+        eligible to apply. <a>Apply Unlock &gt;</a>
+        </Paragraph>
+      </div>
+    </Result>
   );
 }
 
-export default App;
+export default App
