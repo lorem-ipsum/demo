017281c7b82cf48e7857dc82758d5e39e28da0b6
lorem-ipsum <lorem-ipsum@users.noreply.github.com>
2020-06-23 05:43:39 +0000

    Automated deployment: Tue Jun 23 05:43:39 UTC 2020 0178cd879ab20c1eb0428f60176aee23f7afec5c

---
+++ precache-manifest.aa6971ed05adaa6b1d4e960adff9fc0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e667d634dcdd78469b3fa1053d90fe",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "ce643000df507ebf4996",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "faf47e8a0afa599a0917",
+    "url": "/demo/static/js/2.29938b7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.29938b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce643000df507ebf4996",
+    "url": "/demo/static/js/main.405abc31.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
